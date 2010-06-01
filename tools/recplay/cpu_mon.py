4586018fd04b200b417927c24507178e4afdeda2
simon.a.f.lund <simon.a.f.lund@d3e28291-b320-a52d-0db1-01485a0b347e>
2010-06-01 15:34:50 +0000

    Mousing recording and play-back tools added... where else should i put them?

---
+++ tools/recplay/cpu_mon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import subprocess
+
+def sample_cpu(pname):
+  ps = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).communicate()[0]
+  processes = ps.split('\n')
+  # this specifies the number of splits, so the splitted lines
+  # will have (nfields+1) elements
+  nfields = len(processes[0].split()) - 1
+  cpu_total = 0.0
+  mem_total = 0.0
+
+  for row in processes[1:]:
+    r = row.split(None, nfields)
+    if len(row)>0 and pname in r[-1]:
+      cpu_total += float(r[2])
+      mem_total += float(r[3])
+
+  return (cpu_total, mem_total)
